4c15a2f6042fca0520aee7c7a2481e9ec46aa2aa
Eugene Glushchuk <70891544+eugeeenius@users.noreply.github.com>
2020-12-18 20:48:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const app = express()
 const port = 5000
 const parser = bodyParser()
 
-if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
-}
+app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
+
 
 app.use(parser)
 app.use('/api', require('./routes'))
